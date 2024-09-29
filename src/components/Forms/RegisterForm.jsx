6e5448c41464d5d4dc0b4ee2aa52e51badec4f26
tarti <tartilaci@gmail.com>
2024-09-29 23:13:41 +0200

    "Updated validation error messages and added asterisks to required field labels in RegisterForm.jsx"

--- src/components/Forms/RegisterForm.jsx
+++ src/components/Forms/RegisterForm.jsx
@@ -19,16 +19,16 @@ import { useDispatch } from 'react-redux';
 //  aynı olması. Eğer bir harf bile farklı olsa o alanla ilgili yazdığınız
 //  validation çalışmaz.
 export const SignupSchema = Yup.object().shape({
-  username: Yup.string().min(3).max(15).required("Required!"),
+  username: Yup.string().min(3).max(15).required("Username is required!"),
   firstName: Yup.string()
     .min(2, "Too Short!")
     .max(50, "Too Long!")
-    .required("Required"),
+    .required("Firstname is required"),
   lastName: Yup.string()
     .min(2, "Too Short!")
     .max(50, "Too Long!")
-    .required("Required"),
-  email: Yup.string().email("Invalid email").required("Required"),
+    .required("Lastname is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
   password: Yup.string()
     .min(8, "It must be at least 8 characters long!")
     .max(50, "It can be a maximum of 50 characters long!")
@@ -39,10 +39,10 @@ export const SignupSchema = Yup.object().shape({
       /[@$?!%&*]+/,
       "Must contain at least one special character (@$!%*?&)!"
     )
-    .required(),
+    .required("Password is required"),
     confirmPassword: Yup.string()
     .oneOf([Yup.ref('password'), null], 'Passwords must match')
-    .required("It must be at least 8 characters long!")
+    .required("Passwodrs must match!")
 });
 
 const SignUpForm = ({
@@ -75,7 +75,7 @@ const SignUpForm = ({
           <TextField
             id="username"
             name="username" //formik name attributedından eşleştirme yapıyor.
-            label="Username"
+            label="Username*"
             inputProps={{
               autoComplete: "off"  // Burada "autoComplete" kullanılmalıdır.
             }}
@@ -83,12 +83,16 @@ const SignUpForm = ({
             value={values.username}
             onChange={handleChange}
             onBlur={handleBlur} // kullanıcının input alanından ayrıldığını yaklayan event
-            helperText={touched.username && errors.username} //validationda verdiğimiz kalıba uymazsa ilgili mesajları göstermesi için errors dan gelen mesajı yakalıyoruz.
-            error={touched.username && Boolean(errors.username)} //validationda verdiğimiz kalıba uymazsa rengi errora çevirmesi için error attribute ı benden false/true degeri bekliyor ondan dolayı daha sağlıklı olması için boolean deger döndürüyoruz.
+            // helperText={touched.username && errors.username} //validationda verdiğimiz kalıba uymazsa ilgili mesajları göstermesi için errors dan gelen mesajı yakalıyoruz.
+            // error={touched.username && Boolean(errors.username)} //validationda verdiğimiz kalıba uymazsa rengi errora çevirmesi için error attribute ı benden false/true degeri bekliyor ondan dolayı daha sağlıklı olması için boolean deger döndürüyoruz.
             // touched da kullanıcının inputa tıklayıp tıklamadığını yakalıyor
           />
+           <Box sx={{ minHeight: '18px', color: 'red', fontSize: '12px' }}>
+  {touched.username && errors.username ? errors.username : " "}
+</Box>
+
           <TextField
-            label="First Name"
+            label="First Name*"
             name="firstName"
             id="firstName"
             inputProps={{
@@ -100,11 +104,16 @@ const SignUpForm = ({
             value={values.firstName}
             onChange={handleChange}
             onBlur={handleBlur}
-            helperText={touched.firstName && errors.firstName}
-            error={touched.firstName && Boolean(errors.firstName)}
+            // helperText={touched.firstName && errors.firstName}
+            // error={touched.firstName && Boolean(errors.firstName)}
           />
+          <Box sx={{ minHeight: '18px', color: 'red', fontSize: '12px' }}>
+  {touched.firstName && errors.firstName ? errors.firstName : " "}
+</Box>
+
+
           <TextField
-            label="Last Name"
+            label="Last Name*"
             name="lastName"
             id="lastName"
             type="text"
@@ -113,11 +122,15 @@ const SignUpForm = ({
             value={values.lastName}
             onChange={handleChange}
             onBlur={handleBlur}
-            helperText={touched.lastName && errors.lastName}
-            error={touched.lastName && Boolean(errors.lastName)}
+            // helperText={touched.lastName && errors.lastName}
+            // error={touched.lastName && Boolean(errors.lastName)}
           />
+                    <Box sx={{ minHeight: '18px', color: 'red', fontSize: '12px' }}>
+  {touched.lastName && errors.lastName ? errors.lastName : " "}
+</Box>
+
           <TextField
-            label="Email"
+            label="Email*"
             name="email"
             id="email"
             type="email"
@@ -126,11 +139,14 @@ const SignUpForm = ({
             value={values.email}
             onChange={handleChange}
             onBlur={handleBlur}
-            helperText={touched.email && errors.email}
-            error={touched.email && Boolean(errors.email)}
+            // helperText={touched.email && errors.email}
+            // error={touched.email && Boolean(errors.email)}
           />
+            <Box sx={{ minHeight: '18px', color: 'red', fontSize: '12px' }}>
+  {touched.email && errors.email ? errors.email : " "}
+</Box>
           <TextField
-            label="Password"
+            label="Password*"
             name="password"
             id="password"
             type={showPassword ? 'text' : 'password'}
@@ -139,8 +155,8 @@ const SignUpForm = ({
             value={values.password}
             onChange={handleChange}
             onBlur={handleBlur}
-            helperText={touched.password && errors.password}
-            error={touched.password && Boolean(errors.password)}
+            // helperText={touched.password && errors.password}
+            // error={touched.password && Boolean(errors.password)}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
@@ -153,13 +169,16 @@ const SignUpForm = ({
                 </InputAdornment>),
             }}      
           /> 
+                <Box sx={{ minHeight: '18px', color: 'red', fontSize: '12px' }}>
+  {touched.password && errors.password ? errors.password : " "}
+</Box>
           
            {/* <IconButton onClick={handleClickShowPassword}>
               {showPassword ? <VisibilityOff /> : <Visibility />}
             </IconButton> */}
 
           <TextField
-            label="Confirm Password"
+            label="Confirm Password*"
             name="confirmPassword"
             id="confirmPassword"
             type={showConfirmPassword ? 'text' : 'password'}
@@ -168,8 +187,8 @@ const SignUpForm = ({
             value={values.confirmPassword}
             onChange={handleChange}
             onBlur={handleBlur}
-            helperText={touched.confirmPassword && errors.confirmPassword}
-            error={touched.confirmPassword && Boolean(errors.confirmPassword)}
+            // helperText={touched.confirmPassword && errors.confirmPassword}
+            // error={touched.confirmPassword && Boolean(errors.confirmPassword)}
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
@@ -182,6 +201,10 @@ const SignUpForm = ({
                 </InputAdornment>),
             }}  
           />
+          <Box sx={{ minHeight: '18px', color: 'red', fontSize: '12px' }}>
+  {touched.confirmPassword && errors.confirmPassword ? errors.confirmPassword : " "}
+</Box>
+          
 
           <Button
             type="submit"
